25e03172a22a947c4c216f812bb3e7d0c7ad41cf
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-09-10 16:48:56 -0400

    connect database to app

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const express= require('express');
+const Activity = require("../models/activity");
 const router = express.Router();
 
 // endpoints in this file are nested under '/api'
@@ -7,17 +8,21 @@ router.get('/', function(req, res){
   res.json({ key: "This value is a string" });
 })
 
-router.get('/todos', function(req, res){
-  res.json([
-    {
-      item: "Take a nap",
-      complete: false
-    },
-    {
-      item: "Eat some guacamole",
-      complete: false
-    }
-  ]);
+router.get('/activities', function(req, res){
+  Activity.find().then(function (activity){
+    res.render('activities', {activity: activity});
+  })
+
+  // res.json([
+  //   {
+  //     item: "Take a nap",
+  //     complete: false
+  //   },
+  //   {
+  //     item: "Eat some guacamole",
+  //     complete: false
+  //   }
+  // ]);
 });
 
 module.exports = router;
